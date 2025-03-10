2ca0b89af0944dbe7815bc5756afc857df22ca4e
Md Asif Iqbal <117485071+Asif135-hub@users.noreply.github.com>
2025-03-10 21:51:15 +0100

    controller code added

--- app/controllers/artistcontroller.php
+++ app/controllers/artistcontroller.php
@@ -38,6 +38,30 @@ class ArtistController
 
     function addToCart()
     {
+        if (isset($_POST['add-to-cart'])) {
+        if (isset($_SESSION['userId'])) {
+            $user_id = $_SESSION['userId'];
+            $product_id = htmlspecialchars($_POST["product_id"]);
+            $qty = 1;
+
+            $cartItem = new ShoppingCartItem();
+
+            $cartItem->setUser_id($user_id);
+            $cartItem->setProduct_id($product_id);
+            $cartItem->setQty($qty);
+            if ($this->cartService->checkIfProductExistsInCart($user_id, $product_id)) {
+                echo "<script>alert('This product is already in your shopping cart. You can change the quantity in the shopping cart page.')</script>";
+            } else {
+                $this->cartService->addProductToCart($cartItem);
+                $_SESSION['cartcount']++;
+            }
+        } else {
+            echo "<script>
+            alert('You have to be logged in to add to cart.');
+            window.location.href = '/login/index'
+            </script>";
+        }
+    }
         
     }
 
